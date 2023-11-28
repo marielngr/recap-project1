95e9e1626bed54088263cc2e8b4d9686ddc39ab0
Marie Langer <98027776+marielngr@users.noreply.github.com>
2023-11-28 20:34:58 +0100

    add text field counter

--- js/index.js
+++ js/index.js
@@ -69,9 +69,9 @@ addToggleClassListener(
 
 //Form - Submit-button
 
-const formButtonSubmit = document.querySelector('[data-js="button-submit"]');
+const formButtonSubmit = document.querySelector('[data-js="buttonSubmit"]');
 const form = document.querySelector('[data-js="form"]');
-const yourQuestion = document.querySelector("#your-question");
+// const yourQuestion = document.querySelector("#your-question");
 
 if (form !== null) {
   form.addEventListener("submit", (event) => {
@@ -85,15 +85,96 @@ if (form !== null) {
     console.log(event.target.elements);
     console.log(data);
 
-    // Funktionsaufruf für newCard
+    // Funktionsaufruf für newCard mit Argumenten
+    createNewCard(data.question, data.answer, data.tag);
 
     //reset all formfields after submit and refocus
     event.target.reset();
-    event.target.yourQuestion.focus();
+    event.target.elements.question.focus();
   });
 }
 
-//Counter
+//Erstellung der DOM-Elemente für neue Karten + Parameter für Eingabetexte
+
+const mainElement = document.querySelector("body > main");
+
+function createNewCard(question, answer, tag) {
+  const section = document.createElement("section");
+  section.classList.add("qabox");
+
+  const buttonBookmark = document.createElement("button");
+  buttonBookmark.classList.add("button-bookmark");
+  buttonBookmark.setAttribute("data-js", "bookmark-button");
+  section.append(buttonBookmark);
+
+  const pQuestion = document.createElement("p");
+  pQuestion.classList.add("question");
+  pQuestion.textContent = question;
+  section.append(pQuestion);
+
+  const buttonShowAnswer = document.createElement("button");
+  buttonShowAnswer.classList.add("button-show-answer");
+  buttonShowAnswer.setAttribute("data-js", "bookmark-button");
+  buttonShowAnswer.textContent = "Answer";
+  section.append(buttonShowAnswer);
+
+  const spanShow = document.createElement("span");
+  spanShow.classList.add("button-show-answer_show");
+  spanShow.textContent = "Show";
+  buttonShowAnswer.append(spanShow);
+
+  const spanHide = document.createElement("span");
+  spanShow.classList.add("button-show-answer_hide");
+  spanShow.textContent = "Hide";
+  buttonShowAnswer.append(spanHide);
+
+  const pAnswer = document.createElement("p");
+  pAnswer.classList.add("answer");
+  pAnswer.textContent = answer;
+  section.append(pAnswer);
+
+  const ul = document.createElement("ul");
+  ul.classList.add("categories");
+  section.append(ul);
+
+  const li = document.createElement("li");
+  li.textContent = tag;
+  ul.append(li);
+
+  mainElement.append(section);
+}
+
+//Text field counter
+
+const questionInput = document.querySelector("#your-question");
+const characterDisplay = document.querySelector(
+  '[data-js="remaining-characters"]'
+);
+
+questionInput.addEventListener("input", (event) => {
+  const currentCharacterCount = event.target.value.length;
+  characterDisplay.textContent = 150 - currentCharacterCount;
+});
+
+const answerInput = document.querySelector("#your-answer");
+const characterDisplay2 = document.querySelector(
+  '[data-js="remaining-characters2"]'
+);
+
+answerInput.addEventListener("input", (event) => {
+  const currentCharacterCount = event.target.value.length;
+  characterDisplay2.textContent = 150 - currentCharacterCount;
+});
+
+const tagInput = document.querySelector("#tag");
+const characterDisplay3 = document.querySelector(
+  '[data-js="remaining-characters3"]'
+);
+
+tagInput.addEventListener("input", (event) => {
+  const currentCharacterCount = event.target.value.length;
+  characterDisplay3.textContent = 150 - currentCharacterCount;
+});
 
 //Categorien-Keywords?
 
