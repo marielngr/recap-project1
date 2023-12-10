20e30a1c6b7041710cc5cbb5afd76a7a1bd92e6b
Marie Langer <98027776+marielngr@users.noreply.github.com>
2023-12-10 11:47:56 +0100

    create array qaboxes

---
+++ js/data/data.js
@@ -0,0 +1,18 @@
+export const qaboxes = [
+    {
+        question: "What properties flips the axes in flexbox?",
+        answer: "flex-direction";
+        tags: ["#html", "#flexbox", "#css"],
+        isBookmarked: false,
+    },
+    {
+        question: "Lorem Impsum dolor sit?",
+        answer: "Tüdelüdelü";
+        tags: ["#html", "#flexbox", "#css"],
+        isBookmarked: false,
+    },
+]
+
+
+
+
